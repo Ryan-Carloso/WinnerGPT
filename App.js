780b770e0e7940c021358d9d00a96848f95c70f4
Ryan Carlos <114867274+Ryan-Carloso@users.noreply.github.com>
2024-07-30 13:55:11 +0100

    changed the api route

--- App.js
+++ App.js
@@ -17,7 +17,7 @@ export default function App() {
     setLoading(true);
     setError(null);
     try {
-      const response = await axios.get('https://api-alpha-umber.vercel.app/data');
+      const response = await axios.get('https://api-winner-gpt.vercel.app/data');
       setData(response.data);
       setLoading(false);
     } catch (error) {
